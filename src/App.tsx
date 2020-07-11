9ae2bf211b0edeeece81a444347007d056f61667
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-11 15:55:41 +0200

    Set up simple routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import './App.css';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
+    </Switch>
+  );
 }
 
 export default App;
