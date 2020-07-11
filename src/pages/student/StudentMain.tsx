bf3d21aaaf6e05023a6b53d25894a1ed33be84ed
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-11 15:49:11 +0200

    Set up very basic folder structure

---
+++ src/pages/student/StudentMain.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function StudentMain() {
+  return <div>Student Main</div>;
+}
---
+++ src/pages/teacher/TeacherMain.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function TeacherMain() {
+  return <div>Teacher Main</div>;
+}
