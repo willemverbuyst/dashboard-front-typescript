6a836f7271d79783cc39de6d4dbf19ce6f984d98
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-18 16:53:52 +0200

    Set up student main page

--- src/pages/student/StudentMain.tsx
+++ src/pages/student/StudentMain.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectStudentToken } from '../../store/student/selectors';
+import { Layout } from 'antd';
+
+const { Content } = Layout;
 
 export default function StudentMain() {
-  return <div>Student Main</div>;
+  const history = useHistory();
+  const token = useSelector(selectStudentToken);
+
+  useEffect(() => {
+    if (token === null) {
+      history.push('/');
+    }
+  });
+
+  return (
+    <Layout>
+      <Layout style={{ padding: '24px', minHeight: '92vh' }}>
+        <Content className="site-layout-background">Student main</Content>
+      </Layout>
+    </Layout>
+  );
 }
