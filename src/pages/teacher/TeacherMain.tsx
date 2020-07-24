7f54821b3b1ace15b4e3d13949d3b712a20a4b2b
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-24 06:35:15 +0200

    Render pie chart for teacher main

--- src/pages/teacher/TeacherMain.tsx
+++ src/pages/teacher/TeacherMain.tsx
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom';
 
 import { useDispatch, useSelector } from 'react-redux';
 import BarChart from '../../components/charts/BarChart';
-
+import PieChart from '../../components/charts/PieChart';
 import {
   selectTeacherToken,
   selectTeacherId,
@@ -12,7 +12,7 @@ import {
 import { getMainOverview } from '../../store/overviewTeacher/actions';
 import {
   selectMainOverview,
-  // selectMainOverviewScatter,
+  selectMainOverviewScatter,
 } from '../../store/overviewTeacher/selectors';
 import { Layout, Row, Col } from 'antd';
 
@@ -25,6 +25,7 @@ export default function TeacherMain() {
   const id = useSelector(selectTeacherId);
   const mainPageData = useSelector(selectMainOverview);
   const subjects = useSelector(selectTeacherSubjects);
+  const tests = useSelector(selectMainOverviewScatter);
 
   useEffect(() => {
     if (token === null) {
@@ -59,11 +60,38 @@ export default function TeacherMain() {
     }
   };
 
+  const renderPieChart = () => {
+    if (tests && subjects) {
+      const reducedTests = tests.map((test) => test.result);
+      const counts = { '0/3': 0, '1/3': 0, '2/3': 0, '3/3': 0 };
+      reducedTests.forEach((num) => {
+        counts[`${num}/3`] += 1;
+      });
+
+      console.log(reducedTests);
+      return (
+        <Col style={{ width: 450, paddingBottom: 80 }}>
+          <PieChart
+            data={Object.values(counts)}
+            color={['#EEE', '#B81D9D', '#D222E1', '#8F1CB8']}
+            title={'TESTS'}
+            labels={Object.keys(counts)}
+          />
+        </Col>
+      );
+    } else {
+      return <p>YOU DON'T HAVE ENOUGH DATA YET TO DISPLAY TEST RATIO</p>;
+    }
+  };
+
   return (
     <Layout>
       <Layout style={{ padding: '24px', minHeight: '92vh' }}>
         <Content className="site-layout-background">
-          <Row justify="space-around">{renderChartsMain()}</Row>
+          <Row justify="space-around">
+            {renderChartsMain()}
+            {renderPieChart()}
+          </Row>
           <Row justify="space-around"></Row>
         </Content>
       </Layout>
