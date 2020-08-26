7dd07bd55898220578a347053a4bbf6e4c3ca00a
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-26 21:16:12 +0200

    Test reducer teacher add subject

--- src/store/teacher/test/reducer.test.ts
+++ src/store/teacher/test/reducer.test.ts
@@ -44,6 +44,10 @@ const subject = {
   name: 'test_subject',
   id: 1,
 };
+const newSubject = {
+  name: 'test_new_subject',
+  id: 1,
+};
 const student = {
   name: 'test_student',
   id: 1,
@@ -86,6 +90,28 @@ describe('Teacher with token', () => {
       expect(newState.token).not.toBe(initialState.token);
       expect(newState.token).toBe(teacher.token);
       expect(newState).toEqual(teacher);
+      expect(newState).not.toEqual(initialState);
+    });
+  });
+});
+
+describe('Teacher adding subject', () => {
+  const action: AddSubject = {
+    type: ADD_SUBJECT,
+    subject: newSubject,
+  };
+  describe('with state and ADD_SUBJECT action', () => {
+    test('returns the new state with new subject add to subjects', () => {
+      const newState = reducer(teacher, action);
+      expect(newState.token).toBe(teacher.token);
+      expect(newState.subjects).toEqual([subject, newSubject]);
+      expect(newState).not.toEqual(initialState);
+    });
+  });
+  describe('a state with no subjects and ADD_SUBJECT action', () => {
+    test('returns the new state with only one subject', () => {
+      const newState = reducer({ ...teacher, subjects: [] }, action);
+      expect(newState.subjects).toEqual([newSubject]);
       expect(newState).not.toEqual(initialState);
     });
   });
