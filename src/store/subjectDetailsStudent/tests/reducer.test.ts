182c20d31ccaef4ec41292b3ed5661bf1987d7fa
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-27 14:20:33 +0200

    Test remove results for subject reducer

--- src/store/subjectDetailsStudent/tests/reducer.test.ts
+++ src/store/subjectDetailsStudent/tests/reducer.test.ts
@@ -14,16 +14,31 @@ const initialState = {
 
 const subjectDetails: SubjectDetailStudent[] = [{ at: 'test_at', result: 1 }];
 
-const action: ResultsFetched = {
+const action_fetch: ResultsFetched = {
   type: FETCH_RESULTS_FOR_SUBJECT,
   subjectDetails,
 };
 
+const action_remove: RemoveDetailsStudent = {
+  type: REMOVE_RESULTS_FOR_SUBJECT,
+};
+
 describe('When results are fetched', () => {
-  test('dispatch FETCH_RESULTS_FOR_SUBJECT action  ', () => {
-    const newState = reducer(initialState, action);
+  test('dispatch FETCH_RESULTS_FOR_SUBJECT action', () => {
+    const newState = reducer(initialState, action_fetch);
     expect(newState).not.toEqual(initialState);
     expect(newState).not.toBeNull;
     expect(newState.all).toEqual(subjectDetails);
+  });
+});
+
+describe('When all is not null', () => {
+  test('and dispatch REMOVE_RESULTS_FOR_SUBJECT action', () => {
+    const newState = reducer(
+      { ...initialState, all: subjectDetails },
+      action_remove
+    );
+    expect(newState).toEqual(initialState);
+    expect(newState.all).toBeNull;
   });
 });
