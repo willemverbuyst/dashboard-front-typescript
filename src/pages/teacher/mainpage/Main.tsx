a1d39cd80804166cc546809b5832ae2859cc13a5
Willem Verbuyst <willemverbuyst@gmail.com>
2021-05-01 10:08:14 +0200

    Move pie chart to separate component

--- src/pages/teacher/mainpage/Main.tsx
+++ src/pages/teacher/mainpage/Main.tsx
@@ -2,9 +2,6 @@ import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import BarChart from '../../../components/charts/BarChart';
-import PieChart from '../../../components/charts/PieChart';
-
-import LineChartMain from './LineChartMain';
 import {
   selectTeacherToken,
   selectTeacherId,
@@ -16,6 +13,8 @@ import {
 } from '../../../store/overviewTeacher/selectors';
 import { getMainOverview } from '../../../store/overviewTeacher/actions';
 import { Layout, Row, Col } from 'antd';
+import LineChartMain from './LineChartMain';
+import PieChartMain from './PieChartMain';
 import ScatterChartMain from './ScatterChartMain';
 
 const { Content } = Layout;
@@ -62,47 +61,22 @@ export default function TeacherMain() {
     }
   };
 
-  const renderPieChart = () => {
-    if (tests && subjects) {
-      const reducedTests = tests.map((test) => test.result);
-      const counts = { '0/3': 0, '1/3': 0, '2/3': 0, '3/3': 0 };
-      reducedTests.forEach((num) => {
-        counts[`${num}/3`] += 1;
-      });
-
-      return (
-        <Col style={{ width: 450, paddingBottom: 80 }}>
-          <PieChart
-            data={Object.values(counts)}
-            color={['#EEE', '#B81D9D', '#D222E1', '#8F1CB8']}
-            title={'TESTS'}
-            labels={Object.keys(counts)}
-          />
-        </Col>
-      );
-    } else {
-      return <p>YOU DON'T HAVE ENOUGH DATA YET TO DISPLAY TEST RATIO</p>;
-    }
-  };
-
   return (
     <Layout>
       <Layout style={{ padding: '24px', minHeight: '92vh' }}>
         <Content className="site-layout-background">
-          <Row justify="space-around">
-            {renderChartsMain()}
-            {renderPieChart()}
-          </Row>
-          <Row justify="space-around">
-            {tests && subjects ? (
-              <>
+          {tests && subjects ? (
+            <>
+              <Row justify="space-around">
+                {renderChartsMain()}
+                <PieChartMain tests={tests} />
+              </Row>
+              <Row justify="space-around">
                 <LineChartMain tests={tests} />
                 <ScatterChartMain tests={tests} />
-              </>
-            ) : (
-              <></>
-            )}
-          </Row>
+              </Row>
+            </>
+          ) : null}
         </Content>
       </Layout>
     </Layout>
---
+++ src/pages/teacher/mainpage/PieChartMain.tsx
@@ -0,0 +1,29 @@
+import React, { ReactElement } from 'react';
+import PieChart from '../../../components/charts/PieChart';
+import { Col } from 'antd';
+import { ITest } from '../../../models/test.models';
+
+interface IProps {
+  tests: ITest[];
+}
+
+const PieChartMain: React.FC<IProps> = ({ tests }: IProps): ReactElement => {
+  const reducedTests = tests.map((test) => test.result);
+  const counts = { '0/3': 0, '1/3': 0, '2/3': 0, '3/3': 0 };
+  reducedTests.forEach((num) => {
+    counts[`${num}/3`] += 1;
+  });
+
+  return (
+    <Col style={{ width: 450, paddingBottom: 80 }}>
+      <PieChart
+        data={Object.values(counts)}
+        color={['#EEE', '#B81D9D', '#D222E1', '#8F1CB8']}
+        title={'TESTS'}
+        labels={Object.keys(counts)}
+      />
+    </Col>
+  );
+};
+
+export default PieChartMain;
