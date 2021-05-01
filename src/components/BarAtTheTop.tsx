0509ce116c58ade3f82cc1a78cb4d69533adfc8e
Willem Verbuyst <willemverbuyst@gmail.com>
2021-05-01 12:52:26 +0200

    Update BarAtTheTop

--- src/components/BarAtTheTop.tsx
+++ src/components/BarAtTheTop.tsx
@@ -1,54 +1,47 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import moment from 'moment';
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 import { selectStudentName } from '../store/student/selectors';
 import { selectTeacherName } from '../store/teacher/selectors';
 import { Layout } from 'antd';
+import { renderDate } from '../helpers/date.functions';
 
 const { Header } = Layout;
 
-const renderDate = () => {
-  return moment().format('MMMM Do YYYY, dddd');
-};
-
-export default function BarAtTheTop() {
+const BarAtTheTop = (): ReactElement => {
   const student = useSelector(selectStudentName);
   const teacher = useSelector(selectTeacherName);
 
-  const renderLoginLogout = () => {
-    return student || teacher ? <LogoutButton /> : <LoginButton />;
+  const headerStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    minHeight: '8vh',
+    color: '#fff',
   };
 
-  const renderWelcome = () => {
-    return student ? (
+  const linkStyle = {
+    color: '#fff',
+    fontFamily: 'Sriracha',
+    fontSize: '2rem',
+    transform: 'rotate(-5deg)',
+  };
+
+  const renderLoginLogout = (): ReactElement =>
+    student || teacher ? <LogoutButton /> : <LoginButton />;
+
+  const renderWelcome = (): ReactElement | null =>
+    student ? (
       <div>Welcome {student}!</div>
     ) : teacher ? (
       <div>Welcome {teacher}!</div>
     ) : null;
-  };
 
   return (
-    <Header
-      style={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        minHeight: '8vh',
-        color: '#fff',
-      }}
-    >
-      <Link
-        style={{
-          color: '#fff',
-          fontFamily: 'Sriracha',
-          fontSize: '2rem',
-          transform: 'rotate(-5deg)',
-        }}
-        to="/"
-      >
+    <Header style={headerStyle}>
+      <Link style={linkStyle} to="/">
         Dashboard
       </Link>
       {renderDate()}
@@ -56,4 +49,6 @@ export default function BarAtTheTop() {
       {renderLoginLogout()}
     </Header>
   );
-}
+};
+
+export default BarAtTheTop;
---
+++ src/helpers/date.functions.ts
@@ -0,0 +1,3 @@
+import moment from 'moment';
+
+export const renderDate = () => moment().format('MMMM Do YYYY, dddd');
