042dcdaee1364f74a53751454cf16c3e50d2f638
Willem Verbuyst <willemverbuyst@gmail.com>
2021-05-07 07:01:34 +0200

    Refactor tests reducer for test

--- src/store/test/tests/reducer.test.ts
+++ src/store/test/tests/reducer.test.ts
@@ -8,30 +8,29 @@ import {
   TestState,
 } from '../types';
 
-const initialState: TestState = {
-  all: null,
-};
+describe('#testReducer', () => {
+  describe('w/ initial state and FETCH_MC_QUESTIONS action', () => {
+    const initialState: TestState = {
+      all: null,
+    };
+    const MCAnswer = {
+      id: 1,
+      text: 'test_answer',
+      correct: true,
+      questionId: 1,
+    };
 
-const MCAnswer = {
-  id: 1,
-  text: 'test_answer',
-  correct: true,
-  questionId: 1,
-};
+    const MCQuestion = {
+      id: 1,
+      text: 'test_question',
+      subjectId: 1,
+      answers: [MCAnswer],
+    };
+    const action: MC3QuestionsFetched = {
+      type: FETCH_MC_QUESTIONS,
+      mc3questions: [MCQuestion],
+    };
 
-const MCQuestion = {
-  id: 1,
-  text: 'test_question',
-  subjectId: 1,
-  answers: [MCAnswer],
-};
-
-describe('Fetching questions', () => {
-  const action: MC3QuestionsFetched = {
-    type: FETCH_MC_QUESTIONS,
-    mc3questions: [MCQuestion],
-  };
-  describe('with initial state and FETCH_MC_QUESTIONS action', () => {
     test('returns the state with the questions', () => {
       const newState = reducer(initialState, action);
       expect(newState).not.toEqual(initialState);
@@ -39,18 +38,34 @@ describe('Fetching questions', () => {
       expect(newState.all).not.toBeNull();
     });
   });
-});
 
-describe('Removing questions', () => {
-  const action: RemoveQuestions = {
-    type: REMOVE_MC_QUESTIONS,
-  };
-  test('returns the initial state', () => {
+  describe('w/ state and REMOVE_MC_QUESTIONS action', () => {
+    const initialState: TestState = {
+      all: null,
+    };
+    const MCAnswer = {
+      id: 1,
+      text: 'test_answer',
+      correct: true,
+      questionId: 1,
+    };
+    const MCQuestion = {
+      id: 1,
+      text: 'test_question',
+      subjectId: 1,
+      answers: [MCAnswer],
+    };
+    const action: RemoveQuestions = {
+      type: REMOVE_MC_QUESTIONS,
+    };
     const newState_1 = reducer({ ...initialState, all: [MCQuestion] }, action);
-    expect(newState_1).toEqual(initialState);
-    expect(newState_1.all).toBeNull();
     const newState_2 = reducer(initialState, action);
-    expect(newState_2).toEqual(initialState);
-    expect(newState_2.all).toBeNull();
+
+    test('returns the initial state', () => {
+      expect(newState_1).toEqual(initialState);
+      expect(newState_1.all).toBeNull();
+      expect(newState_2).toEqual(initialState);
+      expect(newState_2.all).toBeNull();
+    });
   });
 });
