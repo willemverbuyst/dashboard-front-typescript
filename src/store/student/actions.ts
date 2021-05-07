60ecd0f0a84cf9dbbcd86746324925439e5ea1a2
Willem Verbuyst <willemverbuyst@gmail.com>
2021-05-07 18:22:14 +0200

    Refactor actions student

--- src/store/student/actions.ts
+++ src/store/student/actions.ts
@@ -1,6 +1,6 @@
 import { apiUrl } from '../../constants/environment';
 import axios from 'axios';
-import { Dispatch } from 'redux';
+import { Action, Dispatch } from 'redux';
 import {
   LOGIN_SUCCESS_STUDENT,
   TOKEN_STILL_VALID_STUDENT,
@@ -23,6 +23,8 @@ import {
 import { removeResults } from '../overviewStudent/actions';
 import { removeDetailsStudent } from '../subjectDetailsStudent/actions';
 import { removeQuestions } from '../test/actions';
+import { ThunkAction } from 'redux-thunk';
+import { StoreState } from '../types';
 
 export const loginSuccessStudent = (student: Student): LoginSuccessStudent => {
   return {
@@ -42,101 +44,109 @@ export const logOutStudent = (): LogOutStudent => ({
   type: LOG_OUT_STUDENT,
 });
 
-export const studentLoggingIn = (credentials: LoginCredentials) => {
-  return async (dispatch: Dispatch) => {
-    dispatch(appLoading());
-    try {
-      const { email, password, status } = credentials;
-      const response = await axios.post(`${apiUrl}/login`, {
-        email,
-        password,
-        isStudent: status,
-      });
+export const studentLoggingIn = (
+  credentials: LoginCredentials
+): ThunkAction<void, StoreState, unknown, Action<string>> => async (
+  dispatch: Dispatch
+): Promise<void> => {
+  dispatch(appLoading());
+  try {
+    const { email, password, status } = credentials;
+    const response = await axios.post(`${apiUrl}/login`, {
+      email,
+      password,
+      isStudent: status,
+    });
 
-      dispatch(loginSuccessStudent(response.data));
-      dispatch<any>(
-        showMessageWithTimeout('success', false, 'Welcome back!', 1500)
-      );
-      dispatch(appDoneLoading());
-    } catch (error) {
-      if (error.response) {
-        console.log(error.response.data.message);
+    dispatch(loginSuccessStudent(response.data));
+    showMessageWithTimeout(dispatch, 'success', false, 'Welcome back!', 1500);
 
-        dispatch(setMessage('error', true, error.response.data.message));
-      } else {
-        console.log(error.message);
-        dispatch(setMessage('error', true, error.message));
-      }
-      dispatch(appDoneLoading());
+    dispatch(appDoneLoading());
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.data.message);
+
+      dispatch(setMessage('error', true, error.response.data.message));
+    } else {
+      console.log(error.message);
+      dispatch(setMessage('error', true, error.message));
     }
-  };
+    dispatch(appDoneLoading());
+  }
 };
 
-export const getStudentWithStoredToken = () => {
-  return async (dispatch: Dispatch) => {
-    dispatch(appLoading());
-    try {
-      // if token check if valid
-      const token = localStorage.getItem('student_token');
-      const response = await axios.get(`${apiUrl}/student`, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      dispatch(tokenStudentStillValid(response.data));
-      dispatch(appDoneLoading());
-    } catch (error) {
-      if (error.response) {
-        console.log(error.response.message);
-      } else {
-        console.log(error);
-      }
-      dispatch(logOutStudent());
-      dispatch(appDoneLoading());
+export const getStudentWithStoredToken = (): ThunkAction<
+  void,
+  StoreState,
+  unknown,
+  Action<string>
+> => async (dispatch: Dispatch): Promise<void> => {
+  dispatch(appLoading());
+  try {
+    // if token check if valid
+    const token = localStorage.getItem('student_token');
+    const response = await axios.get(`${apiUrl}/student`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    dispatch(tokenStudentStillValid(response.data));
+    dispatch(appDoneLoading());
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.message);
+    } else {
+      console.log(error);
     }
-  };
-};
-
-export const studentLoggingOut = () => {
-  return function thunk(dispatch: Dispatch) {
     dispatch(logOutStudent());
-    dispatch(removeResults());
-    dispatch(removeDetailsStudent());
-    dispatch(removeQuestions());
-  };
+    dispatch(appDoneLoading());
+  }
 };
 
-export const createStudent = (signUpCredentials: SignUpCredentials) => {
-  const { status, name, email, password, teacherId } = signUpCredentials;
-  return async (dispatch: Dispatch) => {
-    dispatch(appLoading());
-    try {
-      const response = await axios.post(`${apiUrl}/signup`, {
-        isStudent: status,
-        name,
-        email,
-        password,
-        teacherId,
-      });
+export const studentLoggingOut = (): ThunkAction<
+  void,
+  StoreState,
+  unknown,
+  Action<string>
+> => async (dispatch: Dispatch): Promise<void> => {
+  dispatch(logOutStudent());
+  dispatch(removeResults());
+  dispatch(removeDetailsStudent());
+  dispatch(removeQuestions());
+};
 
-      dispatch(loginSuccessStudent(response.data));
-      dispatch<any>(
-        showMessageWithTimeout(
-          'success',
-          true,
-          response.data.message,
-          1500,
-          dispatch
-        )
-      );
-      dispatch(appDoneLoading());
-    } catch (error) {
-      if (error.response) {
-        console.log(error.response.data.message);
-        dispatch(setMessage('error', true, error.response.data.message));
-      } else {
-        console.log(error.message);
-        dispatch(setMessage('error', true, error.message));
-      }
-      dispatch(appDoneLoading());
+export const createStudent = (
+  signUpCredentials: SignUpCredentials
+): ThunkAction<void, StoreState, unknown, Action<string>> => async (
+  dispatch: Dispatch
+): Promise<void> => {
+  const { status, name, email, password, teacherId } = signUpCredentials;
+  dispatch(appLoading());
+  try {
+    const response = await axios.post(`${apiUrl}/signup`, {
+      isStudent: status,
+      name,
+      email,
+      password,
+      teacherId,
+    });
+
+    dispatch(loginSuccessStudent(response.data));
+    showMessageWithTimeout(
+      dispatch,
+      'success',
+      true,
+      response.data.message,
+      1500
+    );
+
+    dispatch(appDoneLoading());
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.data.message);
+      dispatch(setMessage('error', true, error.response.data.message));
+    } else {
+      console.log(error.message);
+      dispatch(setMessage('error', true, error.message));
     }
-  };
+    dispatch(appDoneLoading());
+  }
 };
