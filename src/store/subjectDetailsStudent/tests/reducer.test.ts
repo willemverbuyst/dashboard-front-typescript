bca56664adf56c56b56236c1ad383b0b7562011a
Willem Verbuyst <willemverbuyst@gmail.com>
2021-05-08 16:35:34 +0200

    Refactor tests reducer subject details student

--- src/store/subjectDetailsStudent/tests/reducer.test.ts
+++ src/store/subjectDetailsStudent/tests/reducer.test.ts
@@ -1,44 +1,51 @@
 import reducer from '../reducer';
-
 import {
   FETCH_RESULTS_FOR_SUBJECT,
   REMOVE_RESULTS_FOR_SUBJECT,
   ResultsFetched,
   RemoveDetailsStudent,
   SubjectDetailStudent,
+  SubjectDetailsStudentState,
 } from '../types';
 
-const initialState = {
-  all: null,
-};
-
-const subjectDetails: SubjectDetailStudent[] = [{ at: 'test_at', result: 1 }];
-
-const action_fetch: ResultsFetched = {
-  type: FETCH_RESULTS_FOR_SUBJECT,
-  subjectDetails,
-};
-
-const action_remove: RemoveDetailsStudent = {
-  type: REMOVE_RESULTS_FOR_SUBJECT,
-};
-
-describe('When results are fetched', () => {
-  test('dispatch FETCH_RESULTS_FOR_SUBJECT action', () => {
+describe('#subjectDetailsStudentState', () => {
+  describe('w/ initial state and FETCH_RESULTS_FOR_SUBJECT action', () => {
+    const initialState: SubjectDetailsStudentState = {
+      all: null,
+    };
+    const subjectDetails: SubjectDetailStudent[] = [
+      { at: 'test_at', result: 1 },
+    ];
+    const action_fetch: ResultsFetched = {
+      type: FETCH_RESULTS_FOR_SUBJECT,
+      subjectDetails,
+    };
     const newState = reducer(initialState, action_fetch);
-    expect(newState).not.toEqual(initialState);
-    expect(newState).not.toBeNull;
-    expect(newState.all).toEqual(subjectDetails);
-  });
-});
 
-describe('When result array is not null', () => {
-  test('and dispatch REMOVE_RESULTS_FOR_SUBJECT action', () => {
+    test('returns state with subject details', () => {
+      expect(newState.all).not.toEqual(initialState.all);
+      expect(newState.all).toEqual(subjectDetails);
+    });
+  });
+
+  describe('w/ state and REMOVE_RESULTS_FOR_SUBJECT action', () => {
+    const initialState: SubjectDetailsStudentState = {
+      all: null,
+    };
+    const subjectDetails: SubjectDetailStudent[] = [
+      { at: 'test_at', result: 1 },
+    ];
+    const action_remove: RemoveDetailsStudent = {
+      type: REMOVE_RESULTS_FOR_SUBJECT,
+    };
     const newState = reducer(
       { ...initialState, all: subjectDetails },
       action_remove
     );
-    expect(newState).toEqual(initialState);
-    expect(newState.all).toBeNull;
+
+    test('', () => {
+      expect(newState).toEqual(initialState);
+      expect(newState.all).toBeNull();
+    });
   });
 });
