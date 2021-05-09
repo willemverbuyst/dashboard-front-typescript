d95c99771a3ee73261066738bc428d1804f2a6f0
Willem Verbuyst <willemverbuyst@gmail.com>
2021-05-09 08:03:51 +0200

    Test reducer overviewTeacher

--- src/store/overviewTeacher/tests/reducer.test.ts
+++ src/store/overviewTeacher/tests/reducer.test.ts
@@ -1,9 +1,17 @@
 import reducer from '../reducer';
 import {
   FETCH_OVERVIEW_FOR_MAIN,
+  FETCH_OVERVIEW_FOR_STUDENT,
+  FETCH_OVERVIEW_FOR_SUBJECT,
   Main,
   MainFetched,
   OverviewTeacherState,
+  RemoveOverviewTeacher,
+  REMOVE_OVERVIEW,
+  Student,
+  StudentsFetched,
+  Subject,
+  SubjectsFetched,
 } from '../types';
 
 describe('#overviewTeacheReducerr', () => {
@@ -29,11 +37,92 @@ describe('#overviewTeacheReducerr', () => {
     };
     const newState: OverviewTeacherState = reducer(initialState, action);
 
-    test('returns the new state with results', () => {
+    test('returns the new state with overview for main', () => {
       expect(newState).not.toEqual(initialState);
-    });
-    test('returns the new state with results', () => {
       expect(newState.main).toEqual(overview);
+      expect(newState).toHaveProperty('subjects');
+      expect(newState).toHaveProperty('students');
+    });
+  });
+
+  describe('w/ initial state and FETCH_OVERVIEW_FOR_STUDENT action', () => {
+    const initialState: OverviewTeacherState = {
+      subjects: null,
+      students: null,
+      main: { scores: null, tests: null },
+    };
+    const students: Student[] = [
+      { name: 'test', score: 1, subjectId: 1, tests: 1 },
+    ];
+    const action: StudentsFetched = {
+      type: FETCH_OVERVIEW_FOR_STUDENT,
+      payload: students,
+    };
+    const newState: OverviewTeacherState = reducer(initialState, action);
+
+    test('returns the new state with students', () => {
+      expect(newState).not.toEqual(initialState);
+      expect(newState.students).toEqual(students);
+      expect(newState).toHaveProperty('subjects');
+      expect(newState).toHaveProperty('main');
+    });
+  });
+
+  describe('w/ initial state and FETCH_OVERVIEW_FOR_SUBJECT  action', () => {
+    const initialState: OverviewTeacherState = {
+      subjects: null,
+      students: null,
+      main: { scores: null, tests: null },
+    };
+    const subjects: Subject[] = [
+      { name: 'test', score: 1, subjectId: 1, tests: 1 },
+    ];
+    const action: SubjectsFetched = {
+      type: FETCH_OVERVIEW_FOR_SUBJECT,
+      payload: subjects,
+    };
+    const newState: OverviewTeacherState = reducer(initialState, action);
+
+    test('returns the new state with subjects', () => {
+      expect(newState).not.toEqual(initialState);
+      expect(newState.subjects).toEqual(subjects);
+      expect(newState).toHaveProperty('students');
+      expect(newState).toHaveProperty('main');
+    });
+  });
+
+  describe('w/ initial state and REMOVE_OVERVIEW action', () => {
+    const initialState: OverviewTeacherState = {
+      subjects: null,
+      students: null,
+      main: { scores: null, tests: null },
+    };
+    const overview: Main = {
+      scores: [{ length: 1, result: 1 }],
+      tests: [
+        {
+          subjectId: 1,
+          result: 1,
+          at: 'now',
+        },
+      ],
+    };
+    const state: OverviewTeacherState = {
+      subjects: null,
+      students: null,
+      main: overview,
+    };
+    const action: RemoveOverviewTeacher = {
+      type: REMOVE_OVERVIEW,
+    };
+    const newState: OverviewTeacherState = reducer(state, action);
+
+    test('returns the new state null for main', () => {
+      expect(newState.main).toEqual(initialState.main);
+      expect(newState.main.scores).toBeNull();
+      expect(newState.main.tests).toBeNull();
+      expect(newState).toHaveProperty('students');
+      expect(newState).toHaveProperty('subjects');
     });
   });
 });
